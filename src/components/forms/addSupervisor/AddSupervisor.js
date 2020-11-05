d9fc98987c9c19a612be430434e128279f7a2247
Shubham Lad <shubham.lad@geeksvila.com>
2020-11-05 14:06:16 +0530

    Add supervisor api integration

--- src/components/forms/addSupervisor/AddSupervisor.js
+++ src/components/forms/addSupervisor/AddSupervisor.js
@@ -1,14 +1,49 @@
-import React from "react";
+import React, { useState } from "react";
+import { api } from "../../../api/Axios";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { Loader } from "../../loader/Loader";
 
 export const AddForm = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [loading, setLoading] = useState(false);
+
+  const notifyError = (message) => {
+    toast.error(message);
+  };
+
+  const notifySuccess = (message) => {
+    toast.success(message);
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      setLoading(true);
+      const { data } = await api.post("/v1/institute/supervisor", {
+        name,
+        email,
+      });
+      setName("");
+      setEmail("");
+      setLoading(false);
+      notifySuccess(data.message);
+    } catch (error) {
+      notifyError(error.message);
+    }
+  };
+
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <div className="form-group">
-        <label for="Name">Name</label>
+        <label for="name">Name</label>
         <input
           type="text"
           className="form-control"
-          id="Name"
+          id="name"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
           placeholder="Name"
         />
       </div>
@@ -18,11 +53,17 @@ export const AddForm = () => {
           type="email"
           className="form-control"
           id="Email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
           placeholder="Enter email"
         />
       </div>
-      <button className="btn btn-primary" style={{ width: "100%" }}>
-        Add
+      <button
+        type="submit"
+        className="btn btn-primary"
+        style={{ width: "100%" }}
+      >
+        {loading ? <Loader /> : "ADD"}
       </button>
     </form>
   );
